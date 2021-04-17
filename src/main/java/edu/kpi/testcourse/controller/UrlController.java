ba04d0a79240b5794c8839d80a553429994f0cd3
TymurL <logvinenko814@gmail.com>
2021-04-17 16:24:34 +0300

    Remove json tool

--- src/main/java/edu/kpi/testcourse/controller/UrlController.java
+++ src/main/java/edu/kpi/testcourse/controller/UrlController.java
@@ -1,14 +1,11 @@
 package edu.kpi.testcourse.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import edu.kpi.testcourse.logic.UrlService;
 import edu.kpi.testcourse.model.ErrorResponse;
 import edu.kpi.testcourse.model.UrlShortenRequest;
 import edu.kpi.testcourse.model.UrlShortenResponse;
-import edu.kpi.testcourse.serialization.JsonTool;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Delete;
@@ -32,7 +29,6 @@ public class UrlController {
 
   private static final Logger logger = LoggerFactory.getLogger(UserController.class);
   private final HttpHostResolver httpHostResolver;
-  private final JsonTool json;
   private final UrlService urlService;
 
   /**
@@ -40,13 +36,11 @@ public class UrlController {
    *
    * @param urlService service for work with url
    * @param httpHostResolver micronaut httpHostResolver
-   * @param json JSON serialization tool
    */
   @Inject
-  public UrlController(UrlService urlService, HttpHostResolver httpHostResolver, JsonTool json) {
+  public UrlController(UrlService urlService, HttpHostResolver httpHostResolver) {
     this.urlService = urlService;
     this.httpHostResolver = httpHostResolver;
-    this.json = json;
   }
 
   @Delete("/{alias}")
@@ -60,22 +54,22 @@ public class UrlController {
   /**
    * Create alias for URL.
    */
-  @Post(value = "/shorten", processes = MediaType.APPLICATION_JSON)
-  public HttpResponse<String> shorten(
+  @Post("/shorten")
+  public HttpResponse<?> shorten(
       @Body UrlShortenRequest request,
       Principal principal,
       HttpRequest<?> httpRequest
-  ) throws JsonProcessingException {
+  ) {
+    logger.info("Processing POST /shorten request");
     String email = principal.getName();
     try {
       String baseUrl = httpHostResolver.resolve(httpRequest);
       var shortenedUrl = baseUrl + "/r/"
           + urlService.createNewAlias(email, request.url(), request.alias());
-      return HttpResponse.created(
-        json.toJson(new UrlShortenResponse(shortenedUrl)));
+      return HttpResponse.created(new UrlShortenResponse(shortenedUrl));
     } catch (Exception e) {
       return HttpResponse.serverError(
-        json.toJson(new ErrorResponse(1, "Alias is already taken"))
+        new ErrorResponse(1, "Alias is already taken")
       );
     }
   }
